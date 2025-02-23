be48f171ee77d0faf9cb57316591b6abaf30572a
Ethan3027 <ethandabeal@gmail.com>
2025-02-23 00:08:23 -0600

    MainAsignatura.java

---
+++ MainAsignatura.java
@@ -0,0 +1,16 @@
+public class MainAsignatura {
+    public static void main(String[]usage){
+        Asignatura poo=new Asignatura();
+        poo.setNombre("Programacion Orientada a Objetos");
+        poo.setClave("AED-1286");
+        poo.setCreditos(5);
+        poo.setTeoricas(2);
+        poo.setPracticas(3);
+
+        System.out.println(poo);
+
+
+    }
+
+
+}
