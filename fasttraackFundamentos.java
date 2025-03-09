337e63a2a11cc127ca865bbf5d0bb084c36bce55
Ethan3027 <ethandabeal@gmail.com>
2025-03-08 23:54:16 -0600

    fasttraackFundamentos.java

---
+++ fasttraackFundamentos.java
@@ -0,0 +1,184 @@
+import java.util.Scanner;
+public class fasttraackFundamentos {
+    public static void main(String[] args) {
+       /*
+        // variables en java
+        int a;
+        double b;
+        float c;
+        String d;
+        boolean e;
+
+        a=3;
+        b=20.40;
+        d="Esto es una cadena";
+        e=true;
+        String nombre="Ethan David";
+
+        //constantes
+
+        final double PI=3.1416;
+        //Quitar final para que corra lo de PI
+
+        Scanner intro=new Scanner(System.in);
+
+        System.out.println(b);
+        b=intro.nextDouble();
+        System.out.println(b);
+
+        System.out.println(PI);
+        PI= intro.nextDouble();
+        System.out.println(PI);
+        */
+
+
+        //condicionales <, >, <=, >=, !=, ==, !
+        //operadores logicos &&, ||, !
+        /*
+        int valor = 20;
+        int calif = 82;
+        if ((valor >= 20 || calif > 80)) {
+            if (valor == 20) {
+                System.out.println("Es exactamente 20");
+            } else {
+                System.out.println("Es superior a 20");
+            }else{
+                System.out.println("Es menor o igual");
+            }
+
+            System.out.println("FIN DEL PROGRAMA");
+            */
+
+            /*
+            int opcion;
+            opcion=4;
+
+            switch (opcion){
+                case 1:
+                    System.out.println("Vale 1");
+                    break;
+                case 2:
+                    System.out.println("Vale 2");
+                    break;
+                    case 3:
+                        System.out.println("Vale 3");
+                        break;
+                default:
+                    System.out.println("Valor no permitido");
+                    break;
+
+
+            }*/
+
+        //ciclos:
+        /*
+        for (int i=10; j=0;i!=j;i--,j++;){
+        if (i==9)
+            continue;
+        System.out.println(i);
+        if (i==7) {
+            break;
+            */
+        /*
+        Scanner teclado=new Scanner(System.in);
+        int opcion =-1;
+        for (;;){
+            System.out.println("Escribe un cero para salir ");
+            opcion= teclado.nextInt();
+           switch (opcion){
+               case 1:
+                   System.out.println("Tecleaste 1");
+                   break;
+               case 2:
+                   System.out.println("Tecleaste 2");
+                   break;
+               case 3:
+                   System.out.println("Tecleaste 3");
+                   break;
+               default:
+                   break;
+           }
+           if (opcion==0)
+               break;
+        }//termina el for */
+
+        /*System.out.println(sumar(3,5));
+        multiplica(6,10);*/
+
+        //arreglos
+        //int[] edades={18,20,21,25,19,22};        //declarar  un arreglo
+        //String nombres[]={"Leo","Abraham","Rebeca","Ethan"};
+        /*int edades[]=new int[10];
+        edades[6]=16;
+        edades[1]=20;
+        System.out.println(edades[6]);
+        System.out.println(edades[1]);
+        System.out.println(edades[0]);
+
+        String[] paises=new String[5];
+        paises[0]="Mexico";
+        paises[3]="Suiza";
+        System.out.println(paises[0]);
+        System.out.println(paises[3]);*/
+
+
+        //System.out.println(edades[1]);
+        //System.out.println(nombres[nombres.length-1]);
+        //for (int i=0;i< nombres.length;i++){
+        //  System.out.println(nombres[i]);
+
+    }//fin del main
+}
+        //matrices,son arreglos bidimensionales
+        /*int[][] calificaciones={{1,2,3,},
+                                {4,5,6},
+                                {7,8,9}};
+         System.out.println(calificaciones[0][2]);
+
+        for(int i=0;i< calificaciones.length;i++){
+            for(int j=0;j< calificaciones[0].length;j++ ){
+                System.out.println(calificaciones[i][j]); //"i,j j,i"(o cambiar el orden ya sea fila,columna o columna,fila)
+            }
+    }
+
+        double[][] calif_poo=new double[27][6];
+        calif_poo[6][0]=99
+        System.out.println(calif_poo[6][0]);
+
+
+    }//fin del main
+        //while
+    /*
+    int centinela=20;
+    while(centinela){
+        System.out.println(opcion);
+
+    }
+
+     int centinela=0;
+    do{
+        System.out.println("centinela en do while");
+
+    }
+
+     */
+    /* while(centinela!=0);
+    System.out.println(sumar(3,5))
+    multiplica(6,10) //argumentos
+    }
+
+    public static  int sumar(int a, int b){//parametros
+    return a+b;
+    public static void multiplica(int a, int b){
+        int res;
+        res=x*y;
+            System.out.println(res);
+            */
+
+
+
+
+
+
+
+
